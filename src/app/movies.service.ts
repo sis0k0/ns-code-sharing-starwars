9bb39b2f91dbe1876200a6459aec5b530635f76d
sis0k0 <stanimira.vlaeva@gmail.com>
2018-11-29 18:48:26 +0200

    restore urls

--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -6,67 +6,35 @@ import { Movie } from './movie';
   providedIn: 'root'
 })
 export class MoviesService {
-  // private movies = [
-  //   {
-  //     'title': 'Star Wars: Episode I - The Phantom Menace',
-  //     'poster': 'https://github.com/Package/Star-Wars-Express/blob/master/public/images/star_wars_episode_1_poster.png?raw=true',
-  //   },
-
-  //   {
-  //     'title': 'Star Wars: Episode II - Attack of the Clones',
-  //     'poster': 'https://github.com/Package/Star-Wars-Express/blob/master/public/images/star_wars_episode_2_poster.png?raw=true',
-  //   },
-
-  //   {
-  //     'title': 'Star Wars: Episode III - Revenge of the Sith',
-  //     'poster': 'https://github.com/Package/Star-Wars-Express/blob/master/public/images/star_wars_episode_3_poster.png?raw=true',
-  //   },
-
-  //   {
-  //     'title': 'Star Wars: Episode IV - A New Hope',
-  //     'poster': 'https://github.com/Package/Star-Wars-Express/blob/master/public/images/star_wars_episode_4_poster.png?raw=true',
-  //   },
-
-  //   {
-  //     'title': 'Star Wars: Episode V - The Empire Strikes Back',
-  //     'poster': 'https://github.com/Package/Star-Wars-Express/blob/master/public/images/star_wars_episode_5_poster.png?raw=true',
-  //   },
-
-  //   {
-  //     'title': 'Star Wars: Episode VI - Return of the Jedi',
-  //     'poster': 'https://github.com/Package/Star-Wars-Express/blob/master/public/images/star_wars_episode_6_poster.png?raw=true',
-  //   }
-  // ];
-
   private movies = [
     {
       'title': 'Star Wars: Episode I - The Phantom Menace',
-      'poster': '~/assets/octocat.png',
+      'poster': 'https://github.com/Package/Star-Wars-Express/blob/master/public/images/star_wars_episode_1_poster.png?raw=true',
     },
 
     {
       'title': 'Star Wars: Episode II - Attack of the Clones',
-      'poster': '/assets/octocat.png',
+      'poster': 'https://github.com/Package/Star-Wars-Express/blob/master/public/images/star_wars_episode_2_poster.png?raw=true',
     },
 
     {
       'title': 'Star Wars: Episode III - Revenge of the Sith',
-      'poster': '/assets/octocat.png',
+      'poster': 'https://github.com/Package/Star-Wars-Express/blob/master/public/images/star_wars_episode_3_poster.png?raw=true',
     },
 
     {
       'title': 'Star Wars: Episode IV - A New Hope',
-      'poster': '/assets/octocat.png',
+      'poster': 'https://github.com/Package/Star-Wars-Express/blob/master/public/images/star_wars_episode_4_poster.png?raw=true',
     },
 
     {
       'title': 'Star Wars: Episode V - The Empire Strikes Back',
-      'poster': '/assets/octocat.png',
+      'poster': 'https://github.com/Package/Star-Wars-Express/blob/master/public/images/star_wars_episode_5_poster.png?raw=true',
     },
 
     {
       'title': 'Star Wars: Episode VI - Return of the Jedi',
-      'poster': '/assets/octocat.png',
+      'poster': 'https://github.com/Package/Star-Wars-Express/blob/master/public/images/star_wars_episode_6_poster.png?raw=true',
     }
   ];
 
